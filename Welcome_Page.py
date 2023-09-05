ac6288e3104e65d6062e27c6bb1bba2e38917f4f
threaces <114806392+threaces@users.noreply.github.com>
2023-09-05 21:56:51 +0200

    Add files via upload
    Script responsible for creating the main page of application

---
+++ Welcome_Page.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title = "Lego application", layout="wide")
+
+st.sidebar.success("Menu")
+
+
+image = Image.open("E:\data_science_pierwsze_Lekcje\lego_price_checker\images\welcome_image_2-removebg-preview.png")
+
+st.markdown(f"<h1 style='text-align: center; color: white;'>Lego Price Checker Application</h1>", unsafe_allow_html=True)
+
+col1, col2 = st.columns([0.65, 0.35])
+
+with col1:
+    st.image(image)
+
+with col2:
+    st.write(f"Purpose of this project is to get information in realtime about concrete LEGO sets like price etc. from different polish shops.")
+    st.write(f"Data are collected once per day from Monday to Friday")
+    st.write(f"Data are avaiable only for sets which are released since 2021")
+    st.subheader("Made by: Korzystka Piotr")
+    st.subheader(f"Link to github: https://github.com/threaces")
